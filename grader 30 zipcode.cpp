1813b150be0159b9e5fad2d8508ba33df419eaa7
6430003421-koraphat-nickname-is-nine <111551189+6430003421-koraphat-nickname-is-nine@users.noreply.github.com>
2022-09-01 21:31:44 +0700

    Create grader 30 zipcode.cpp

---
+++ grader 30 zipcode.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+class Letter{
+    public:
+    string name; string address; string province; string district; int zip;
+    bool operator <(const Letter& other)const {
+        if ( this->zip != other.zip){
+            return this->zip < other.zip;
+        }
+        else{
+            if(this->address != other.address){
+                return this->address < other.address;
+            }
+            else {return this->name < other.name; }
+        }
+    //**Begin Insert**
+    //**End Insert**
+    }
+    };
+    class ZipInfo{
+    public:
+    int zip;
+    string province;
+    string district;
+    };
+void correctZipAndSortLetters(vector<ZipInfo> &zipinfo, vector<Letter> &letters) {
+ //**Begin Insert**
+    for(auto &l: letters){
+        bool found = false;
+        int i = 0;
+        while(!found){
+            if (l.zip != zipinfo[i].zip && l.province == zipinfo[i].province && l.district == zipinfo[i].district){
+                found = true;
+                l.zip = zipinfo[i].zip;
+            }
+            i++;
+        }
+    }
+    sort(letters.begin(),letters.end());
+ //**End Insert**
+}
+int main() {
+    int nzip;
+    cin>>nzip;
+    vector<ZipInfo> zipinfo;
+    for (int i = 0; i < nzip; i++) {
+        ZipInfo z;
+        cin>>z.zip>>z.district>>z.province;
+        zipinfo.push_back(z);
+    }
+    int n;
+    cin>>n;
+    vector<Letter> letters;
+    for (int i = 0; i < n; i++) {
+    Letter l;
+    cin>>l.name>>l.address>>l.district>>l.province>>l.zip;
+    letters.push_back(l);
+    }
+    correctZipAndSortLetters(zipinfo, letters);
+    for (auto& l:letters) {
+    cout<<l.name<<" "<<l.address<<" "<<l.district<<" "<<l.province<<" "<<l.zip<<endl;
+    }
+}
