28c00ab29514e9a5e111d53d2c4639c1f11ef8d0
6430003421-koraphat-nickname-is-nine <111551189+6430003421-koraphat-nickname-is-nine@users.noreply.github.com>
2022-09-14 20:30:03 +0700

    Create g37.cpp

---
+++ g37.cpp
@@ -0,0 +1,8 @@
+/*int k = oldsize;
+  for(int i =0;i < oldsize ;i++){
+    size_t it = k +i;
+    int j = (newsize - 1) - k -i;
+    //at(k+i) = at(j);
+    iterator itinsert = begin() + j;
+    T value = *itinsert;
+    insert_by_pos(it, value);*/
